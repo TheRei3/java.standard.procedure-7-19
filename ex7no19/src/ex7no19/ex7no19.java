4536f44e7c933f0319844bb0a557425f25f91fff
admin <admin@DESKTOP-COIHK7E>
2021-08-17 07:40:02 +0900

    ex7no19

---
+++ ex7no19/src/ex7no19/ex7no19.java
@@ -0,0 +1,25 @@
+package ex7no19;
+
+public class ex7no19 {  // 365p
+	public static void main(String[] args) {
+		Parent2 p = new Child2();
+		Child2 c = new Child2();
+		
+		System.out.println("p.x = " + p.x);
+		p.method();
+		
+		System.out.println("c.x = " + c.x);
+		c.method();
+	}
+
+}
+
+class Parent2{
+	int x = 100;
+	
+	void method() {
+		System.out.println("Parent Method");
+	}
+}
+
+class Child2 extends Parent2 { }
